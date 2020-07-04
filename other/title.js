0c13c0d4049662b30c684892f3ac8d4c0ec61f3c
jaslli <1141950370@qq.com>
2020-07-04 17:09:44 +0800

    a

--- other/title.js
+++ other/title.js
@@ -15,6 +15,9 @@ document.addEventListener('visibilitychange', function () {
     }
 });
 
+
+
+
 /* eslint-disable */
 (function ($) {
     "use strict";
@@ -52,3 +55,18 @@ document.addEventListener('visibilitychange', function () {
   })(jQuery);
 
 
+
+
+
+
+
+  /*手机默认不展开*/
+var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
+  "menus_item_child"
+);
+var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
+for (var i = 0; i < menus_item_child.length; i++) {
+  menus_item_child[i].style.display = "none";
+  menus_expand[i].className += " menus-closed";
+}
