01cc3e0a35f060fb72226778bf7bcd9c4941d0b7
jaslli <1141950370@qq.com>
2020-10-10 17:31:06 +0800

    修改了一些样式

--- other/main.js
+++ other/main.js
@@ -29,3 +29,37 @@ document.addEventListener('visibilitychange', function() {
 $(document).ready(function(e) {
     $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Yw');
 });
+
+
+//网站运行时间
+//在layout/includes/footer.pug下增加#running-time
+setInterval(() => {
+    let create_time = Math.round(new Date("2019-10-31-00:00:00").getTime() / 1000);
+    let timestamp = Math.round((new Date().getTime() + 8 * 60 * 60 * 1000) / 1000);
+    let second = timestamp - create_time;
+    let time = new Array(0, 0, 0, 0, 0);
+    if (second >= 365 * 24 * 3600) {
+        time[0] = parseInt(second / (365 * 24 * 3600));
+        second %= 365 * 24 * 3600
+    }
+    if (second >= 24 * 3600) {
+        time[1] = parseInt(second / (24 * 3600));
+        second %= 24 * 3600
+    }
+    if (second >= 3600) {
+        time[2] = parseInt(second / 3600);
+        second %= 3600
+    }
+    if (second >= 60) {
+        time[3] = parseInt(second / 60);
+        second %= 60
+    }
+    if (second > 0) {
+        time[4] = second
+    }
+    currentTimeHtml = '小破站已经安全运行 ' + time[0] + ' 年 ' + time[1] + ' 天 ' + time[2] + ' 时 ' + time[3] + ' 分 ' + time[4] + ' 秒';
+    var elementById = document.getElementById('running-time');
+    if (elementById) {
+        elementById.innerHTML = currentTimeHtml
+    }
+}, 1000); 
