8aba2e0ab0e53bee81c5446c2ba11483efcd0d79
jaslli <1141950370@qq.com>
2020-11-08 19:15:15 +0800

    移除了网站运行时间

--- other/main.js
+++ other/main.js
@@ -31,36 +31,3 @@ $(document).ready(function(e) {
 });
 
 
-//网站运行时间
-//在layout/includes/footer.pug下增加#running-time
-setInterval(() => {
-    //2019年10月31号起
-    let create_time = Math.round(new Date(18200*24*60*60*1000).getTime() / 1000);
-    let timestamp = Math.round((new Date().getTime() + 8 * 60 * 60 * 1000) / 1000);
-    let second = timestamp - create_time;
-    let time = new Array(0, 0, 0, 0, 0);
-    if (second >= 365 * 24 * 3600) {
-        time[0] = parseInt(second / (365 * 24 * 3600));
-        second %= 365 * 24 * 3600
-    }
-    if (second >= 24 * 3600) {
-        time[1] = parseInt(second / (24 * 3600));
-        second %= 24 * 3600
-    }
-    if (second >= 3600) {
-        time[2] = parseInt(second / 3600);
-        second %= 3600
-    }
-    if (second >= 60) {
-        time[3] = parseInt(second / 60);
-        second %= 60
-    }
-    if (second > 0) {
-        time[4] = second
-    }
-    currentTimeHtml = '小破站已经安全运行 ' + time[0] + ' 年 ' + time[1] + ' 天 ' + time[2] + ' 时 ' + time[3] + ' 分 ' + time[4] + ' 秒';
-    var elementById = document.getElementById('running-time');
-    if (elementById) {
-        elementById.innerHTML = currentTimeHtml
-    }
-}, 1000);
