f1d27a8729b253f6daa1d612dab9f39782ab92dc
jaslli <1141950370@qq.com>
2020-11-22 15:13:59 +0800

    增加了gulp和pwa

--- other/main.js
+++ other/main.js
@@ -1,6 +1,3 @@
-/* 手机客户端导航栏默认隐藏 */
-
-
 // 标题变化
 var OriginTitle = document.title;
 var titleTime;
@@ -22,5 +19,55 @@ document.addEventListener('visibilitychange', function() {
 $(document).ready(function(e) {
     $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Yw');
 });
+
+//  头像变化
+var avatar = document.querySelector("#aside_content .card-info img");
+    avatar.onmouseover = function () {
+      avatar.style.cssText = "animation: bigger 2s;width:115px; height:115px; border:3px solid #ff4000; padding:3px;";
+    }
+    avatar.onmouseout = function () {
+        avatar.style.cssText = "animation: smaller 2s;width:105px; height:105px;";
+    }
+
+//  pwa
+  if ('serviceWorker' in navigator) {
+    if (navigator.serviceWorker.controller) {
+      navigator.serviceWorker.addEventListener('controllerchange', function () {
+        showNotification()
+      })
+    }
+
+    window.addEventListener('load', function () {
+      navigator.serviceWorker.register('/sw.js')
+    })
+  }
+
+  function showNotification() {
+    if (GLOBAL_CONFIG.Snackbar) {
+      var snackbarBg =
+        document.documentElement.getAttribute('data-theme') === 'light'
+          ? GLOBAL_CONFIG.Snackbar.bgLight
+          : GLOBAL_CONFIG.Snackbar.bgDark
+      var snackbarPos = GLOBAL_CONFIG.Snackbar.position
+      Snackbar.show({
+        text: '已更新最新版本',
+        backgroundColor: snackbarBg,
+        duration: 500000,
+        pos: snackbarPos,
+        actionText: '点击刷新',
+        actionTextColor: '#fff',
+        onActionClick: function (e) {
+          location.reload()
+        },
+      })
+    } else {
+      var showBg =
+        document.documentElement.getAttribute('data-theme') === 'light'
+          ? '#49b1f5'
+          : '#1f1f1f'
+      var cssText = `top: 0; background: ${showBg};`
+      document.getElementById('app-refresh').style.cssText = cssText
+    }
+  }
 
 
