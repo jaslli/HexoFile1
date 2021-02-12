8654e9a3082b17caae23a83d77b9b84b0e381238
jaslli <1141950370@qq.com>
2021-02-13 04:06:13 +0800

    UPDATE

--- other/main.js
+++ other/main.js
@@ -1,24 +1,16 @@
 // 标题变化
-var OriginTitle = document.title;
-var titleTime;
+let OriginTitle = document.title;
+let titleTime;
 document.addEventListener('visibilitychange', function() {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/favicon.ico");
+        document.innerHTML = '<link rel="icon" href="images/favicon.ico.png"/>';
         document.title = 'aya 崩溃啦';
         clearTimeout(titleTime);
     } else {
-        $('[rel="icon"]').attr('href', "/img/favicon.ico");
+        document.innerHTML = '<link rel="icon" href="images/favicon.ico.png"/>';
         document.title = 'ya 又好了';
         titleTime = setTimeout(function() {
             document.title = OriginTitle;
         }, 2000);
     }
 });
-
-// 页脚博主
-$(document).ready(function(e) {
-    $('.copyright').html('©2021 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Yw');
-});
-
-
-
